e493c39888040eb1d171016582a4a7d30fb671e3
Salako Oluwadolapo <willdynamics@gmail.com>
2023-01-29 12:10:05 +0100

    Update meta.py

--- meta.py
+++ meta.py
@@ -6,7 +6,7 @@ SIDEBAR_INFO = """
         </div>
 """
 CHEF_INFO = """
-        <h2 class="font-title">Hello, I am Babbage! </h2>
+        <h2 class="font-title">DraftAssist: Your Personal Writing Assistant! </h2>
         <p class="strong font-body">
         <span class="d-block extra-info">(Select your preferred parameters or leave them as default to get started.)</span>
         </p>
@@ -15,9 +15,11 @@ ROMPT_BOX = "Add custom ingredients here (separated by `,`): "
 STORY = """
 <div class="story-box font-body">
 <p>
-Hello everyone 👋, I am <strong>Babbage</strong>, 
-"I am good at picking up obvious patterns in text and then using that as a reference to generate text. \
-                I can also perform broad classification tasks like assigning categories to industries, genres and media content. \
-                    For creative applications, I am able to understand enough structure to create simple plots and titles."</p>
+Hello everyone 👋, I am <strong>DraftAssist</strong>, 
+"I Draft Assist is an AI-powered writing assistant that utilizes the power of GPT-3 to help you quickly write high-quality documents. \
+\n\n Draft Assist helps you create drafts quickly and easily, with natural language processing and deep learning that can detect and correct mistakes. \
+\n\n With Draft Assist, you can generate content based on existing drafts, making it easier to produce polished, on-brand content quickly. \
+\n\n You can also use Draft Assist to check your grammar, find synonyms, and even rephrase entire sentences or sections. Draft Assist can help you save time and resources while producing excellent writing. \
+\n\n Human: The above writeup itself was written by DraftAssist 😀."</p>
 </div>
 """.strip()
